7c318eedd4321d9af2274d6cc18cfd1686a28917
unknown <you@example.com>
2021-12-17 01:00:18 +0330

    done

--- index.php
+++ index.php
@@ -1,8 +1,22 @@
 <?php
-$question = 'این یک پرسش نمونه است';
-$msg = 'این یک پاسخ نمونه است';
-$en_name = 'hafez';
-$fa_name = 'حافظ';
+
+$content=json_decode(file_get_contents("people.json"),true);
+if (sizeof($_POST)>0) {
+
+$arrans= file("messages.txt", FILE_IGNORE_NEW_LINES);
+$question=$_POST["question"];
+$seed=intval(hash("adler32",$question),16)%sizeof($content);
+$seed=$seed+intval(hash("adler32",$_POST["person"]),16)%sizeof($content);
+$seed=$seed%sizeof($content);
+$msg = $arrans[$seed];
+$en_name = $_POST["person"];
+$fa_name = $content[$en_name];}
+else{
+    $msg="سوال خود را بپرس!";
+    $question='';
+    $en_name=array_rand($content,1);
+    $fa_name=$content[$en_name];
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -12,12 +26,19 @@ $fa_name = 'حافظ';
     <title>مشاوره بزرگان</title>
 </head>
 <body>
+
 <p id="copyright">تهیه شده برای درس کارگاه کامپیوتر،دانشکده کامییوتر، دانشگاه صنعتی شریف</p>
 <div id="wrapper">
-    <div id="title">
-        <span id="label">پرسش:</span>
-        <span id="question"><?php echo $question; ?></span>
-    </div>
+    <?php
+    if(sizeof($_POST)){
+    echo ("<div id=\"title\">");
+     echo ( " <span id=\"label\">پرسش:</span>");
+    echo ("<span id=\"question\"><?php echo $question ?></span>");
+    echo ("</div>");
+
+    }
+    ?>
+
     <div id="container">
         <div id="message">
             <p><?php echo $msg ?></p>
@@ -32,14 +53,24 @@ $fa_name = 'حافظ';
     <div id="new-q">
         <form method="post">
             سوال
-            <input type="text" name="question" value="<?php echo $question; ?>" maxlength="150" placeholder="..."/>
+            <input type="text" name="question" value="<?php echo $question ?>" maxlength="150" placeholder="..."/>
             را از
             <select name="person">
+
                 <?php
-                echo "<option value=\"hafez\">حافظ</option>";
-        
+
+                foreach ($content as $key=> $value) {
+                    if ($key==$en_name) {
+                        echo "<option value=\"".$key."\""."selected".">".$value."</option>";
+                    }else{
+                    echo "<option value=\"".$key."\">".$value."</option>";}
+                }
+                /*
+                 * Loop over people data and
+                 * enter data inside `option` tag.
+                 * E.g., <option value="hafez">حافظ</option>
+                 */
                 ?>
-                <option value="hafez">حاظ</option>
             </select>
             <input type="submit" value="بپرس"/>
         </form>
