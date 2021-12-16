7c07d0f77b9cfd1d5839f38794528df3a3334266
unknown <you@example.com>
2021-12-17 00:58:59 +0330

    done

---
+++ index.php
@@ -0,0 +1,49 @@
+<?php
+$question = 'این یک پرسش نمونه است';
+$msg = 'این یک پاسخ نمونه است';
+$en_name = 'hafez';
+$fa_name = 'حافظ';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="styles/default.css">
+    <title>مشاوره بزرگان</title>
+</head>
+<body>
+<p id="copyright">تهیه شده برای درس کارگاه کامپیوتر،دانشکده کامییوتر، دانشگاه صنعتی شریف</p>
+<div id="wrapper">
+    <div id="title">
+        <span id="label">پرسش:</span>
+        <span id="question"><?php echo $question; ?></span>
+    </div>
+    <div id="container">
+        <div id="message">
+            <p><?php echo $msg ?></p>
+        </div>
+        <div id="person">
+            <div id="person">
+                <img src="images/people/<?php echo "$en_name.jpg" ?>"/>
+                <p id="person-name"><?php echo $fa_name ?></p>
+            </div>
+        </div>
+    </div>
+    <div id="new-q">
+        <form method="post">
+            سوال
+            <input type="text" name="question" value="<?php echo $question; ?>" maxlength="150" placeholder="..."/>
+            را از
+            <select name="person">
+                <?php
+                echo "<option value=\"hafez\">حافظ</option>";
+        
+                ?>
+                <option value="hafez">حاظ</option>
+            </select>
+            <input type="submit" value="بپرس"/>
+        </form>
+    </div>
+</div>
+</body>
+</html>
